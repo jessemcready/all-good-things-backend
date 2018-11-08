d83863f7680bb290b4910f5a4040b012fac0f209
Jesse McReady <jessemcready@gmail.com>
2018-11-08 11:31:06 -0500

    added dependent destroys for user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: { case_sensitive: false }
 
-  has_many :posts
-  has_many :likes
-  has_many :comments
-  has_many :relationships
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :relationships, dependent: :destroy
   has_many :followers, through: :relationships, :foreign_key => :follower_id
   has_many :followees, through: :relationships, :foreign_key => :followee_id
 
