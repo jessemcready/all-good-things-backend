49430a6021752024d65c3514154d157c9b143727
Jesse McReady <jessemcready@gmail.com>
2018-11-18 16:05:19 -0500

    post comments rendered by newest

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,7 +2,13 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :content, :flagged, :created_at, :updated_at
   belongs_to :user
   has_many :likes
-  has_many :comments, serializer: CommentSerializer
+  has_many :comments
+
+  def comments
+    object.comments.sort do |x, y|
+      y.id <=> x.id
+    end
+  end
 
   # def user
   #   foundUser = User.all.find do |user|
